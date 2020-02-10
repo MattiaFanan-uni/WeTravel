20c8638f5d8dd9c2a23bfcbf1c71ae652f8c7915
MattiaFanan-Uni <mattia.fanan@studenti.unipd.it>
2020-02-10 14:02:44 +0100

    dummy implementation of savable NetDictionary and SubscribersList

--- app/src/main/java/com/gruppo3/wetravel/LauncherActivity.java
+++ app/src/main/java/com/gruppo3/wetravel/LauncherActivity.java
@@ -1,11 +1,16 @@
 package com.gruppo3.wetravel;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eis.smsnetwork.SMSJoinableNetManager;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -16,6 +21,39 @@ public class LauncherActivity extends AppCompatActivity {
 
         // Programatically UI initialization
         initUI();
+
+        //initialize Net
+        SMSJoinableNetManager.getInstance().setup(this);
+
+        //TODO nomefileDict and nomfileList go on res/values/strings.xml
+        //try to retrieve the saved dict
+        try {
+            ObjectInputStream streamReader = new ObjectInputStream(this.openFileInput("nomefileDict.extension"));
+            SMSJoinableNetManager.getInstance().setNetDictionary(new DummyResDict(streamReader));
+        } catch (IOException | ClassNotFoundException e) {
+            SMSJoinableNetManager.getInstance().setNetDictionary(new DummyResDict());
+        }
+
+        //try to retrieve the saved subscList
+        try {
+            ObjectInputStream streamReader = new ObjectInputStream(this.openFileInput("nomefileList.extension"));
+            SMSJoinableNetManager.getInstance().setNetSubscriberList(new DummySubscriberList(streamReader));
+        } catch (IOException | ClassNotFoundException e) {
+            SMSJoinableNetManager.getInstance().setNetSubscriberList(new DummySubscriberList());
+        }
+
+        //info-----
+        //TODO use ((DummyResDict)SMSJoinableNetManager.getInstance().getNetDictionary()).save(ObjectOutputStream); to save
+        //throws IOException
+
+        //TODO use SMSJoinableNetManager.getInstance().setNetSubscriberList(new DummySubscriberList(ObjectInputStream)); to retrieve
+        //throws IOException, ClassNotFoundException
+
+        //TODO to build an output stream // new ObjectStreamWriter(context.openFileOutput("nomefile", Context.MODE_PRIVATE));
+
+        //TODO same for NetSubscriberList
+
+        //TODO the ResDict is <String,String> so we can use phonenumber(key), LatLng parsed to string (Value)
     }
 
     /**
@@ -28,8 +66,8 @@ public class LauncherActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
 
         // Adding icon in each TextView
-        TextView[] textViewArray = new TextView[] { findViewById(R.id.textViewtUsername), findViewById(R.id.textViewPassword) };
-        int[] iconsResId = new int[] { R.drawable.user, R.drawable.password };
+        TextView[] textViewArray = new TextView[]{findViewById(R.id.textViewtUsername), findViewById(R.id.textViewPassword)};
+        int[] iconsResId = new int[]{R.drawable.user, R.drawable.password};
         for (int i = 0; i < textViewArray.length; i++) {
             textViewArray[i].setCompoundDrawablesWithIntrinsicBounds(iconsResId[i], 0, 0, 0);
             textViewArray[i].setCompoundDrawablePadding(32);
@@ -38,6 +76,7 @@ public class LauncherActivity extends AppCompatActivity {
 
     /**
      * Checks user input and logs into the system if there isn't any issue.
+     *
      * @param v Clicked view
      */
     public void buttonLogin_onClick(View v) {
